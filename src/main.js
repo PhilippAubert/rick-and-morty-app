e0587f036e058224a845bb884f5634654570b4b7
Philipp Aubert <p.b.m.aubert@gmail.com>
2021-03-27 14:52:10 +0100

    add select clearing the main not working

--- src/main.js
+++ src/main.js
@@ -1,52 +1,62 @@
 console.clear();
 
-const url = "https://rickandmortyapi.com/api/character";
-
 const selectButton = document.querySelector(".dropdown-menu");
 
+let url = "https://rickandmortyapi.com/api/character/";
+
+if (selectButton.value === "alive") {
+  url = `https://rickandmortyapi.com/api/character?status=alive`;
+}
+if (selectButton.value === "dead") {
+  url = "https://rickandmortyapi.com/api/character?status=dead";
+}
+if (selectButton.value === "unknown") {
+  url = "https://rickandmortyapi.com/api/character?status=unknown";
+}
+
 const loadButton = document.querySelector(".button");
+
 loadButton.addEventListener("click", () => {
   fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-
-    .catch((error) => {
-      console.log(error.toString());
-      console.log(error);
-    });
-
-  const apiGET = fetch(url);
-
-  apiGET
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        h1.textContent = response.status;
+        throw Error("ERROR!");
       }
     })
-    .then((character) => {
-      character.results.forEach((residents) => {
-        const main = document.querySelector("main");
 
+    .then((data) => {
+      console.log(data.results);
+
+      data.results.forEach((card) => {
+        const main = document.querySelector("main");
         const section = document.createElement("section");
         section.classList.add("content");
         const textFrame = document.createElement("div");
         textFrame.classList.add("text-container");
         const name = document.createElement("h2");
-        name.textContent = `${residents.name}`;
-        name.classList.add("headline");
+        name.textContent = `${card.name}`;
+        name.classList.add("headline-text");
         const status = document.createElement("p");
-        status.textContent = `STATUS: ${residents.status}`;
-        status.classList.add("status");
+        status.textContent = `${card.status}`;
+        status.classList.add("status-text");
+
+        if (status.textContent === "Alive") {
+          section.classList.add("text-container-alive");
+        } else if (status.textContent === "Dead") {
+          section.classList.add("text-container-dead");
+        } else {
+          section.classList.add("text-container-unknown");
+        }
+
         const location = document.createElement("p");
-        location.textContent = `LOCATION: ${residents.location.name}`;
-        location.classList.add("location");
+        location.textContent = `${card.location.name}`;
+        location.classList.add("location-text");
         const img = document.createElement("img");
         img.classList.add("photo");
-        img.src = residents.image;
-        img.alt = `${residents.name}`;
+        img.src = card.image;
+        img.alt = `${card.name}`;
 
         main.append(section);
         section.append(img);
@@ -54,6 +64,14 @@ loadButton.addEventListener("click", () => {
         textFrame.append(name);
         textFrame.append(status);
         textFrame.append(location);
+
+        loadButton.addEventListener("click", () => {
+          const removeCards = document.querySelector("section");
+          removeCards.forEach((card) => card.remove());
+        });
       });
+    })
+    .catch((error) => {
+      console.log(error.toString());
     });
 });
